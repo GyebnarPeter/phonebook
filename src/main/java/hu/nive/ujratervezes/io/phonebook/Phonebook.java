6140d4b5a7d30fd4c4acced8970e731f6b83bc65
GyebnarPeter <gyebnarpetter@gmail.com>
2021-04-29 11:36:13 +0200

    complete task

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            FileWriter fileWriter = new FileWriter(output);
+            for (String contact : contacts.keySet()) {
+                fileWriter.write(contact + ": " + contacts.get(contact) + System.lineSeparator());
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
